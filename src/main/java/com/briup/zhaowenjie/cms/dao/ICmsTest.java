9114cb85d4346248a60ddfe9d9f2efb568868175
zhaowj <1246063744@qq.com>
2019-11-22 16:51:13 +0800

    试一试

---
+++ src/main/java/com/briup/zhaowenjie/cms/dao/ICmsTest.java
@@ -0,0 +1,9 @@
+package com.briup.zhaowenjie.cms.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.briup.zhaowenjie.cms.bean.CmsTest;
+
+public interface ICmsTest extends JpaRepository<CmsTest,Integer> {
+
+}
