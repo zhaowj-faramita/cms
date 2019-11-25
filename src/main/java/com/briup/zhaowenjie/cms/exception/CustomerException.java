e9ac4f522185ccc56480d802fe1545dd3fcc2949
zhaowj <1246063744@qq.com>
2019-11-25 10:08:09 +0800

    'excption'

---
+++ src/main/java/com/briup/zhaowenjie/cms/exception/CustomerException.java
@@ -0,0 +1,29 @@
+package com.briup.zhaowenjie.cms.exception;
+
+/**
+ * @program: estore
+ * @description: 自定义异常
+ * @author: wangzh
+ **/
+
+public class CustomerException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 状态码
+     */
+    private Integer code;
+
+    public CustomerException(Integer code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
