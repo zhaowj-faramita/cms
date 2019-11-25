9922893f8e676e67606060ad05d69cd7172ca651
zhaowj <1246063744@qq.com>
2019-11-25 09:44:18 +0800

    '9:44'

--- src/main/java/com/briup/zhaowenjie/cms/web/controller/ForWardController.java
+++ src/main/java/com/briup/zhaowenjie/cms/web/controller/ForWardController.java
@@ -1,4 +1,4 @@
-package com.briup.zhaowenjie.cms.web;
+package com.briup.zhaowenjie.cms.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
