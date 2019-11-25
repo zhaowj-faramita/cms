536dd4a2adaefe5b54e1f130fbc37eab542aa55b
zhaowj <1246063744@qq.com>
2019-11-25 09:41:31 +0800

    'change'

--- src/main/java/com/briup/zhaowenjie/cms/dao/ICmsTest.java
+++
@@ -1,9 +0,0 @@
-package com.briup.zhaowenjie.cms.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.briup.zhaowenjie.cms.bean.CmsTest;
-
-public interface ICmsTest extends JpaRepository<CmsTest,Integer> {
-
-}
